02bd49b949e74d0d388a85cc8bccd38e927e5d6d
Erik Schwerdt <erikschwerdt18@gmail.com>
2025-03-26 01:35:35 -0300

    Fix | ENVS - configservice exchange

--- src/providers/exchange-rate/exchange-rate.provider.ts
+++ src/providers/exchange-rate/exchange-rate.provider.ts
@@ -1,16 +1,20 @@
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
 import { RateInterface } from 'src/interfaces/providers/exchange-rate.interface';
 import { AxiosError } from 'axios';
 
 @Injectable()
 export class ExchangeRateProvider {
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+  ) {}
 
   async getPrice(from: string, to: string): Promise<number> {
     try {
       const { data, status } = await this.httpService.axiosRef.get<RateInterface>(
-        `${process.env.EXCHANGE_API_URL}/price/rate?from=${from}&to=${to}`
+        `${this.configService.get('EXCHANGE_API_URL')}/price/rate?from=${from}&to=${to}`
       );
 
       if (status !== 200) {
